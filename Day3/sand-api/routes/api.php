672b4cb96ce51f65a9ecd8052da8f8ae268d345c
Bagas <banabda@gmail.com>
2020-08-27 20:29:06 +0700

    test

--- Day3/sand-api/routes/api.php
+++ Day3/sand-api/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::namespace('Auth')->group(function () {
+    Route::post('register', 'RegisterController');
+    Route::post('login', 'LoginController');
 });
+
+Route::get('user', 'UserController');
