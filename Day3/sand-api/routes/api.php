6240c2d5ab9f6a7ef845afee9725809225478202
Bagas <banabda@gmail.com>
2020-08-27 21:20:20 +0700

    logout

--- Day3/sand-api/routes/api.php
+++ Day3/sand-api/routes/api.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Auth')->group(function () {
     Route::post('register', 'RegisterController');
     Route::post('login', 'LoginController');
+    Route::post('logout', 'LogoutController');
 });
 
 Route::get('user', 'UserController');
